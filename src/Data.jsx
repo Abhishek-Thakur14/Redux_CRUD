9e9262b4c59656baf31cba4abadbfd6b3ac64165
Abhishek kumar <thecode.abhi@gmail.com>
2025-02-27 19:47:48 +0530

    iNitial coMMit

---
+++ src/Data.jsx
@@ -0,0 +1,18 @@
+export const userList = [
+    {
+        name: "Test",
+        email: "test1@gmail.com",
+        id: 1
+    },
+    {
+        name: "Abhi",
+        email: "abhi@gmail.com",
+        id: 2
+    },
+    {
+        name: "Wow",
+        email: "wow@gmail.com",
+        id: 3
+    }
+  
+];
